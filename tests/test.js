85d451ef12f2c1e842412087c879f6bbdf604359
hollisd09 <hollisd09@gmail.com>
2016-01-26 12:23:50 -0600

    Start of the tests. Yay

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,40 @@
 'use strict';
 
 const { expect } = require('chai');
+const { execSync } = require('child_process');
 
-describe('test suite', () => {
-  it('true should be true', () => {
-    expect(true).to.be.true;
+describe('cal', () => {
+  describe('CLI', () => {
+    it('should handle the current month', () => {
+      const goal = execSync('cal').toString();
+      const output = execSync('./cal.js').toString();
+
+      expect(output).to.equal(goal);
+    });
+  });
+
+  describe("Zellar's congruence", () => {
+    // const zellars = require('../zellars.js');
+
+    describe('.modifiedMonth', () => {
+      it('return 13 for January', () => {
+        const mod = zellars.modifiedMonth(2012, 01);
+
+        expect(mod).to.equal(13);
+      });
+      // 2012, 2 === 14
+      // 2012, 3 === 3
+    });
+    // .modified Year
+    // 2000, 1 === 1999
+    // 2012, 2 === 2011
+    // 2013, 3 === 2013
+
+    // .calculate
+    // 2014, 3, 2, === 1
+    // 2012, 1, 1 === 1
+    // 2013, 1, 12 === 5
+    // 1799, 2, 1 === 6
+    // 200, 11, 01 === 4
   });
 });
